186ddfd244619d7718139034e5d76614fa2b80c4
ReetuChauhann <chauhanreetu00@gmail.com>
2022-05-13 12:50:44 +0530

    linear search using recusrion

--- linearsearchrecursion.java
+++ linearsearchrecursion.java
@@ -1,8 +1,11 @@
 public class linearsearchrecursion {
     public static void main(String[] args) {
-        int[] arr={1,2,3,4,7,18};
+        int[] arr={1,2,3,4,18,7};
         boolean ans=exist(arr,18,0);
         System.out.println(ans);
+        int ans2=existat(arr, 18, 0);
+        System.out.println(ans2);
+
     }
     
     static boolean exist(int[] arr,int target, int index){
@@ -10,5 +13,18 @@ public class linearsearchrecursion {
             return false;
         }
         return arr[index]==target||exist(arr, target, index+1);
+    }
+
+    static int existat(int[] arr,int target, int index){
+        if(index==arr.length){
+            return -1;
+        }
+    if(arr[index]==target){
+        return index;
+    }else{
+       return existat(arr, target, index+1);
+    }
+
+       
     }
 }
