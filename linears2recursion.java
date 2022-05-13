8fd3b0a2108b124d4c53c8368dad6d1cfb7390b5
ReetuChauhann <chauhanreetu00@gmail.com>
2022-05-13 13:38:25 +0530

    linear search using arraylist with recursion

--- linears2recursion.java
+++ linears2recursion.java
@@ -4,8 +4,11 @@ import java.util.List;
 public class linears2recursion {
     public static void main(String[] args) {
         int[] arr={1,2,9,4,5,5,5,7};
-        linaersearch(arr, 5, 0);
-        System.out.println(l);
+       // linaersearch(arr, 5, 0);
+       // System.out.println(l);
+        ArrayList<Integer> listp=new ArrayList<>();
+        List<Integer> list5= linaersearch2(arr, 5, 0, listp);
+        System.out.println(list5);
 
     }
     static List<Integer> l=new ArrayList<>();
@@ -20,5 +23,17 @@ public class linears2recursion {
            linaersearch(arr, target, index+1);
         
     }
+
+    static List<Integer> linaersearch2(int[] arr, int target, int index, ArrayList<Integer> list){
+        if(index==arr.length){
+         return list;
+        }
+       
+        if(arr[index]==target){
+          list.add(index);
+        }
+        return linaersearch2(arr, target, index+1, list);
+      
+  }
     
 }
